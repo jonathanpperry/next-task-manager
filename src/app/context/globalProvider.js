c5755f5d5fcb481de2c40629d5f0de185cad4ffe
Jonathan Perry <jpper@umich.edu>
2024-10-11 13:31:21 -0500

    Add null check for data

--- src/app/context/globalProvider.js
+++ src/app/context/globalProvider.js
@@ -36,7 +36,8 @@ export const GlobalProvider = ({ children }) => {
 
     try {
       const res = await axios.get("/api/tasks");
-      const sorted = res?.data.sort((a, b) => {
+      console.log('res.data: ', res.data);
+      const sorted = res?.data?.sort((a, b) => {
         return (
           new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
         );
